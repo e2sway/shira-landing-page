d84c3e137ecd4faca404fb5f26303010db4f5071
Eytan Gipi <egipi18@gmail.com>
2025-02-12 14:26:11 -0800

    updated next.config.js

--- project/next.config.js
+++ project/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'your-domain.com'], // Add your domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
